4280b39ceae5111937700b75df05ff9b9becf53a
Cecilia <ceciliasoares0@gmail.com>
2021-07-26 13:38:21 -0300

    transition and blur

--- Example/PopLoverKit/POPViewController.m
+++ Example/PopLoverKit/POPViewController.m
@@ -7,23 +7,44 @@
 //
 
 #import "POPViewController.h"
+#import "PopLoverKit-umbrella.h"
 
 @interface POPViewController ()
 
 @end
 
+
 @implementation POPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+    UIColor *colour = [[UIColor alloc]initWithRed:57.0/255.0 green:156.0/255.0 blue:52.0/255.0 alpha:1.0];
+    self.view.backgroundColor = colour;
+    UIButton *but= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [but addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [but setFrame:CGRectMake(52, 252, 215, 40)];
+    [but setTitle:@"button" forState:UIControlStateNormal];
+    [but setExclusiveTouch:YES];
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+     // if you like to add backgroundImage else no need
+//       [but setbackgroundImage:[UIImage imageNamed:@"XXX.png"] forState:UIControlStateNormal];
+
+    [self.view addSubview:but];
+
+
 }
+- (void) buttonClicked:(UIButton*)sender
+ {
+     //NSLog(@"you clicked on button %ld", (long)sender.tag);
+     PopPicker *controler = [[PopPicker alloc] init];
+     UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:controler];
+//     [self.navigationController pushViewController: controler animated:YES];
+    nc.modalPresentationStyle = UIModalPresentationOverFullScreen;
+         nc.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+         [self presentViewController:nc animated:YES completion:nil];
+     
+
+ }
+
 
 @end
--- PopLoverKit.m
+++ PopLoverKit.m
@@ -6,3 +6,29 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PopLoverKit.h"
+@interface PopPicker ()
+
+@end
+
+
+@implementation PopPicker
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    if (!UIAccessibilityIsReduceTransparencyEnabled()) {
+        self.view.backgroundColor = [UIColor clearColor];
+
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+        UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+        //always fill the view
+        blurEffectView.frame = self.view.bounds;
+        blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+        [self.view addSubview:blurEffectView]; //if you have more UIViews, use an insertSubview API to place it where needed
+    } else {
+        self.view.backgroundColor = [UIColor blackColor];
+    }
+
+}
+@end
